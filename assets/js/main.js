440ce0ef81ceffa1e0b67c58f651aa428380b07e
agos <arbeitmanpilar@gmail.com>
2025-06-05 01:09:26 -0300

    ⋆｡𖦹°⭒˚｡⋆
    - Arregle el error
    - Agregue un poco de diseño
    - Agregue clases a los textos y card

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,25 +16,29 @@ myData
     })
 
 function makeCards (films){
-    const {title, original_title, image,id}= films
+    const {image, title, original_title,id}= films
     const cardsContainer =document.getElementById("cards-container");
-
-    const name=document.createElement("h3");
-    name.textContent=title;
-
-    const originalName=document.createElement("h4");
-    originalName.textContent=original_title;
 
     const imageFilm=document.createElement("img");
     imageFilm.src=image;
+
+    const name=document.createElement("h3");
+    name.textContent=title;
+    name.setAttribute("class","zen-maru-gothic-regular");
+
+    const originalName=document.createElement("h4");
+    originalName.textContent=original_title;
+    originalName.setAttribute("class","zen-maru-gothic-regular");
    
-    imageFilm.setAttribute("class","image-film")
+    imageFilm.setAttribute("class","image-film");
     const card=document.createElement("div");
      //le asigno la id de cada card para luego acceder a cada uno sin tener que hacer denuevo la llamada a la api
     card.setAttribute("id",id)
+    card.setAttribute("class","cursorPointer");
+    card.appendChild(imageFilm);
     card.appendChild(name);
     card.appendChild(originalName);
-    card.appendChild(imageFilm);
+    
     //card.style.backgroundColor="blue";
 
    
@@ -50,7 +54,7 @@ function pagcard(){
     
     //guardo id en localstorage asi se conserva la informacion
     localStorage.setItem("posicion",this.getAttribute("id"));
-    window.location.href="card"
+    window.location.href="card.html"
 }
 
 
